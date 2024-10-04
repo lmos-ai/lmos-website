b2afab92e24e23f7e77c3ababa297374207c5943
Kai Kreuzer <kai.kreuzer@telekom.de>
2024-10-04 16:43:19 +0200

    Small fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Open-source, cloud-native platform for building and running Multi-Agent systems">
       <HomepageHeader />
       <main>
         <Features /> 
